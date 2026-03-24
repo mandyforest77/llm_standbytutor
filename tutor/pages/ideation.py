22bf1116a46fb74d3c7cc9502282f739b4647064
mandyforest77 <mandyforest77@gmail.com>
2026-03-24 11:31:37 +0900

    Update ideation.py

--- tutor/pages/ideation.py
+++ tutor/pages/ideation.py
@@ -18,5 +18,18 @@ from langchain_core.prompts import PromptTemplate
 from langchain_classic.chains import LLMChain
 from langchain_core.documents import Document # Added import
 from langchain_classic.chains.summarize import load_summarize_chain # Added import
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
 
 st.set_page_config(page_title="Ideation", layout="wide")
+# 2. 클래스 초기화
+# model을 "dall-e-3"로 지정하면 더 고품질의 이미지가 생성됩니다.
+dalle = DallEAPIWrapper(model="dall-e-3")
+
+# 3. 이미지 생성 실행
+# 결과값으로 이미지 파일이 아닌 '이미지 URL' 문자열이 반환됩니다.
+prompt = st.text_input("이미지 생성 실행을 위해 엔터를 눌러주세요.")
+if prompt:
+    image_url = dalle.run(prompt)
+
+# 4. 결과 출력
+    st.image(image_url)
